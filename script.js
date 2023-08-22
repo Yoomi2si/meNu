1a13480844209b1afda71011cb1255ab4ed2ded2
Yoomi2si <140560807+Yoomi2si@users.noreply.github.com>
2023-08-22 23:25:48 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,53 @@
+const helpToggler1 = document.querySelector(".help_toggler1");
+const helpContent1 = document.querySelector(".help_content1");
+
+helpToggler1.addEventListener("click", toggleHelp1);
+
+function toggleHelp1() {
+  helpContent1.classList.toggle("active");
+}
+
+const helpToggler2 = document.querySelector(".help_toggler2");
+const helpContent2 = document.querySelector(".help_content2");
+
+helpToggler2.addEventListener("click", toggleHelp2);
+
+function toggleHelp2() {
+  helpContent2.classList.toggle("active");
+}
+
+const helpToggler3 = document.querySelector(".help_toggler3");
+const helpContent3 = document.querySelector(".help_content3");
+
+helpToggler3.addEventListener("click", toggleHelp3);
+
+function toggleHelp3() {
+  helpContent3.classList.toggle("active");
+}
+
+const helpToggler4 = document.querySelector(".help_toggler4");
+const helpContent4 = document.querySelector(".help_content4");
+
+helpToggler4.addEventListener("click", toggleHelp4);
+
+function toggleHelp4() {
+  helpContent4.classList.toggle("active");
+}
+
+const helpToggler5 = document.querySelector(".help_toggler5");
+const helpContent5 = document.querySelector(".help_content5");
+
+helpToggler5.addEventListener("click", toggleHelp5);
+
+function toggleHelp5() {
+  helpContent5.classList.toggle("active");
+}
+
+const helpToggler6 = document.querySelector(".help_toggler6");
+const helpContent6 = document.querySelector(".help_content6");
+
+helpToggler6.addEventListener("click", toggleHelp6);
+
+function toggleHelp6() {
+  helpContent6.classList.toggle("active");
+}
